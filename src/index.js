a38281c6f370e5414e6e82ff453d1123eeb4a4a2
Mikhail Druzhinin <pini86@mail.ru>
2022-01-04 17:55:42 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    
+    let str = String ( Math.abs (n) );
+
+    let strout = '';
+
+    for ( let i=0; i < str.length; i++ ) {
+      strout =str[i] + strout;
+    }
+
+    return +strout;
 }
